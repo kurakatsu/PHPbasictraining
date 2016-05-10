5877a401a9535369a2459af065064cb2f9d0b05d
kurachi <paseri0406@gmail.com>
2016-05-10 12:28:20 +0900

    for文、if文、switch文

---
+++ 20160510/010_switch.php
@@ -0,0 +1,15 @@
+<?php
+$x = 'xyz';
+switch($x){
+    case 1:
+    echo "値は１だね <br />";
+    break;
+    case 'xyz':
+    case 3:
+   echo "値はxyzか3だね。<br />";
+   break;
+ default:
+   echo "1でも2でも3でもないね。<br />";
+
+}
+?>
